b5850be3fa7832664a4e9f7f517204666de6e92d
PikoNeko <50704408+PikoNeko@users.noreply.github.com>
2024-07-10 21:47:59 +0300

    Исправлена ошибка в тесте на второе граничное значение. Убраны блоки try/catch. Исправлены небольшие огрехи.

--- src/test/java/BoundariesTest.java
+++ src/test/java/BoundariesTest.java
@@ -1,71 +1,67 @@
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
 import static org.testng.Assert.assertEquals;
 
 import static com.lesson10.Lesson10.factorialCalculation;
+
 import com.exceptions.OutOfBoundariesException;
 
 import java.math.BigInteger;
 
+//Тестируются граничные значения метода вычисления факториала
 public class BoundariesTest {
 
+
     @Test(
-            description = "-1",
+
+            description = "-1 - тест на выход за первое граничное значение",
             expectedExceptions = OutOfBoundariesException.class,
-            expectedExceptionsMessageRegExp = "Нельзя вычислять факториал отрицательных чисел!")
+            expectedExceptionsMessageRegExp = "Нельзя вычислять факториал отрицательных чисел!"
+    )
     void belowMinValueTest() throws OutOfBoundariesException {
+
         factorialCalculation(-1);
     }
 
-        @Test(description = "0")
-        @Parameters("minValue")
-        void MinValueTest(int value) {
-            try {
-                assertEquals(BigInteger.valueOf(value), factorialCalculation(0));
-            } catch (OutOfBoundariesException e) {
-                throw new RuntimeException(e);
-            }
+    @Test(description = "0 - первое граничное значение")
+    @Parameters("minValue")
+    void minValueTest(int value) throws OutOfBoundariesException {
 
-        }
+        assertEquals(BigInteger.valueOf(value), factorialCalculation(0));
+    }
 
-        @Test(description = "1")
-        @Parameters("aboveMinValue")
-        void aboveMinValueTest(String value) {
-            try {
-                BigInteger localValue = new BigInteger(value);
-                assertEquals(localValue, factorialCalculation(1));
-            } catch (OutOfBoundariesException e) {
-                throw new RuntimeException(e);
-            }
-        }
+    @Test(description = "1 - выше первого граничного значения")
+    @Parameters("aboveMinValue")
+    void aboveMinValueTest(String value) throws OutOfBoundariesException {
 
-        @Test(description = "99")
-        @Parameters("belowMaxValue")
-        void belowMaxValueTest(String value) {
-            try {
-                BigInteger localValue = new BigInteger(value);
-                assertEquals(localValue, factorialCalculation(99));
-            } catch (OutOfBoundariesException e) {
-                throw new RuntimeException(e);
-            }
-        }
+        BigInteger localValue = new BigInteger(value);
+        assertEquals(localValue, factorialCalculation(1));
+    }
 
-        @Test(description = "100")
-        @Parameters("maxValue")
-        void maxValueTest( String value) {
-            try {
-                BigInteger localValue = new BigInteger(value);
-                assertEquals(localValue, factorialCalculation(100));
-            } catch (OutOfBoundariesException e) {
-                throw new RuntimeException(e);
-            }
-        }
+    @Test(description = "99 - перед вторым граничным значением")
+    @Parameters("belowMaxValue")
+    void belowMaxValueTest(String value) throws OutOfBoundariesException {
 
-        @Test(
-                description = "-1",
-                expectedExceptions = OutOfBoundariesException.class,
-                expectedExceptionsMessageRegExp = "Слишком большое число!")
-        void aboveMaxValueTest() throws OutOfBoundariesException {
-            factorialCalculation(101);
-        }
+        BigInteger localValue = new BigInteger(value);
+        assertEquals(localValue, factorialCalculation(99));
+
+    }
+
+    @Test(description = "100 - второе граничное значение")
+    @Parameters("maxValue")
+    void maxValueTest(String value) throws OutOfBoundariesException {
+
+        BigInteger localValue = new BigInteger(value);
+        assertEquals(localValue, factorialCalculation(100));
+    }
+
+    @Test(
+            description = "-1 - выход за второе граничное значение",
+            expectedExceptions = OutOfBoundariesException.class,
+            expectedExceptionsMessageRegExp = "Слишком большое число!"
+    )
+    void aboveMaxValueTest() throws OutOfBoundariesException {
+        factorialCalculation(101);
+    }
 }
