a5d2fe8d1482f83a831539e3ee8097a9cf377d8b
PikoNeko <50704408+PikoNeko@users.noreply.github.com>
2024-07-10 21:51:37 +0300

    Убраны блоки try/catch. Исправлены небольшие огрехи.

--- src/test/java/BoundariesTest.java
+++ src/test/java/BoundariesTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.math.BigInteger;
 
 import static com.lesson10.Lesson10.factorialCalculation;
+
 import com.exceptions.OutOfBoundariesException;
 
 @DisplayName("Граничное тестирование (0-100)")
@@ -24,23 +25,17 @@ class BoundariesTest {
         @Tag("0")
         @ParameterizedTest
         @ValueSource(ints = {1})
-        void MinValueTest(int value) {
-            try {
-                Assertions.assertEquals(BigInteger.valueOf(value), factorialCalculation(0));
-            } catch (OutOfBoundariesException e) {
-                throw new RuntimeException(e);
-            }
+        void minValueTest(int value) throws OutOfBoundariesException {
+
+            Assertions.assertEquals(BigInteger.valueOf(value), factorialCalculation(0));
         }
 
         @Tag("1")
         @ParameterizedTest
         @ValueSource(ints = {1})
-        void aboveMinValueTest(int value) {
-            try {
-                Assertions.assertEquals(BigInteger.valueOf(value), factorialCalculation(1));
-            } catch (OutOfBoundariesException e) {
-                throw new RuntimeException(e);
-            }
+        void aboveMinValueTest(int value) throws OutOfBoundariesException {
+
+            Assertions.assertEquals(BigInteger.valueOf(value), factorialCalculation(1));
         }
     }
 
@@ -52,13 +47,10 @@ class BoundariesTest {
         @ValueSource(
                 strings = {"933262154439441526816992388562667004907159682643816214685929638952175999932299156089414639761565182862536979208272237582511852109168640000000000000000000000"}
         )
-        void belowMaxValueTest( String value) {
-            try {
-                BigInteger localValue = new BigInteger(value);
-                Assertions.assertEquals(localValue, factorialCalculation(99));
-            } catch (OutOfBoundariesException e) {
-                throw new RuntimeException(e);
-            }
+        void belowMaxValueTest(String value) throws OutOfBoundariesException {
+
+            BigInteger localValue = new BigInteger(value);
+            Assertions.assertEquals(localValue, factorialCalculation(99));
         }
 
         @Tag("100")
@@ -66,13 +58,10 @@ class BoundariesTest {
         @ValueSource(
                 strings = {"93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000"}
         )
-        void maxValueTest( String value) {
-            try {
-                BigInteger localValue = new BigInteger(value);
-                Assertions.assertEquals(localValue, factorialCalculation(100));
-            } catch (OutOfBoundariesException e) {
-                throw new RuntimeException(e);
-            }
+        void maxValueTest(String value) throws OutOfBoundariesException {
+
+            BigInteger localValue = new BigInteger(value);
+            Assertions.assertEquals(localValue, factorialCalculation(100));
         }
 
         @Tag("101")
